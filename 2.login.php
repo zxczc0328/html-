ed285f08c15122362be72e12690e6812af9f5f3c
zxczc0328 <63779149+zxczc0328@users.noreply.github.com>
2020-11-06 14:37:43 +0800

    Add files via upload

---
+++ 2.login.php
@@ -0,0 +1,7 @@
+<?php
+    //輸入帳號密碼
+    echo $_GET[id];
+    echo "<br>";
+    echo $_GET[pwd];
+
+?>
---
+++ 3.login(v.2).php
@@ -0,0 +1,8 @@
+<?php
+
+if (($_Get[id] == "john") && ($_Get[pwd] == "john1234" ))
+        echo "welcome";
+    else
+        echo "fail login" ;
+
+?>
